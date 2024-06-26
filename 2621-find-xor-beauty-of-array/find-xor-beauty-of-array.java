7ef77661fb03ea6dba0bc4906401ad0ca66543eb
saisurya <133490620+Saisurya2234@users.noreply.github.com>
2024-06-26 18:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (50.50%) - LeetSync

---
+++ 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int xorBeauty(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=nums[i];
+        }
+        return ans;  
+        
+    }
+}
