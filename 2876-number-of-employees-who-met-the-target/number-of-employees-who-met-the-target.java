30fcc6f4b761e7afbb8cfd34344e03474ed0e1f9
saisurya <133490620+Saisurya2234@users.noreply.github.com>
2024-07-23 08:01:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.92%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(int i=0;i<hours.length;i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        // for(int i:hours){
+        //     if(i>=target){
+        //         count++;
+        //     }
+        // }
+         return count;
+    }   
+}
