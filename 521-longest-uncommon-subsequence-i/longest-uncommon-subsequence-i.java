ee87a2d21a108753024a060dca9f94823a0db6a7
saisurya <133490620+Saisurya2234@users.noreply.github.com>
2024-08-13 19:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (79.95%) - LeetSync

---
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(!a.equals(b)){
+            return Math.max(a.length(),b.length());
+        }
+        return -1;
+        
+    }
+}
