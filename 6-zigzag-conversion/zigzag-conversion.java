fdc90b6d2708ad98edc4b2d157bdb2a8f1ed6d1d
saisurya <133490620+Saisurya2234@users.noreply.github.com>
2024-08-17 19:58:33 +0530

    Time: 4 ms (81.33%) | Memory: 45.3 MB (45.12%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,32 +1,32 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
+        if(numRows==1){
             return s;
         }
-        
-        StringBuilder answer = new StringBuilder();
-        int n = s.length();
-        int diff = 2 * (numRows - 1);
-        int diagonalDiff = diff;
-        int secondIndex;
-        int index;
-        for (int i = 0; i < numRows; i++) {
-            index = i;
-
-            while (index < n) {
-                answer.append(s.charAt(index));
-                if (i != 0 && i != numRows - 1) {
-                    diagonalDiff = diff-2*i;
-                    secondIndex = index + diagonalDiff;
-                    
-                    if (secondIndex < n) {
-                        answer.append(s.charAt(secondIndex));
-                    }
-                }
-                index += diff;
+        StringBuilder[] sb=new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++){
+            sb[i]=new StringBuilder();
+        }
+        int a=0;
+        int m=numRows;
+        boolean flag=false;
+        for(char c:s.toCharArray()){
+            sb[a].append(c);
+            if(a==0|| a==m-1){
+                flag=!flag;
+            }
+            if(flag){
+                a=a+1;
+            }else{
+                a=a-1;
             }
         }
+        StringBuilder sd=new StringBuilder();
+        for(StringBuilder str:sb){
+            sd.append(str);
+        }
+        return sd.toString();
         
-        return answer.toString();
+        
     }
 }
