1925abe8afe2f9e0e9d1891a74ab44ca8b874492
saisurya <133490620+Saisurya2234@users.noreply.github.com>
2025-02-10 10:32:56 +0530

    Time: 6 ms (98.31%) | Memory: 45.8 MB (5.08%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean sumGame(String n) {
+
+        char[] c = n.toCharArray();
+        int d=0, q=0;
+        for(int i=0; i<c.length/2; i++){
+            if(c[i]=='?')q++;
+            else d-=c[i]-'0';
+        }
+        for(int i=c.length/2; i<c.length; i++){
+            if(c[i]=='?'){d+=9; q++;}
+            else d+=c[i]-'0';
+        }
+        if(q%2==1)return true;
+        if(q==0)return d!=0;
+        return d!=(q/2)*9;
+    }
+}
+     
+        // int n = num.length();
+        
+        // int leftSum = 0, rightSum = 0;
+        
+        // int leftQuestionCount = 0, rightQuestionCount = 0;
+        
+        // for (int i = 0; i < n / 2; i++) {
+        //     char ch = num.charAt(i);
+        //     if (ch == '?') {
+        //         leftQuestionCount++; 
+        //         leftSum += 9; 
+        //     } else {
+        //         leftSum += ch - '0'; 
+        //     }
+        // }
+        
+      
+        // for (int i = n / 2; i < n; i++) {
+        //     char ch = num.charAt(i);
+        //     if (ch == '?') {
+        //         rightQuestionCount++;
+        //         rightSum += 9; 
+        //     } else {
+        //         rightSum += ch - '0'; 
+        //     }
+        // }
+        
+       
+        // return leftSum == rightSum;
+
